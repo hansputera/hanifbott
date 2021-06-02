1ce2a277660400d37920b8a3cf61512fee2a7aba
hansputera <hanif@tslab.site>
2021-06-02 13:27:01 +0000

    fix: exec command error message 2

--- src/commands/developers/ExecCommand.ts
+++ src/commands/developers/ExecCommand.ts
@@ -8,9 +8,9 @@ export default class ExecCommand implements ICommand {
     public ownerOnly = true;
     public async execute(ctx: CTX, args: string[]) {
         if (!args.length) return await ctx.reply("Mohon masukan argumen yang akan dieksekusi!", { reply_to_message_id: ctx.message.message_id });
-        exec(args.join(" "), async (error, stdout, stderr) => {
-            if (error || stderr) return await ctx.replyWithMarkdown("```" + error.message.length ? error.message : "..." || stderr.length ? stderr : "..." + "```", { reply_to_message_id: ctx.message.message_id });
-            else await ctx.replyWithMarkdown(`\`\`\`${stdout.length ? stdout : "Executed"}\`\`\``, { reply_to_message_id: ctx.message.message_id });
+        exec(args.join(" "), (error, stdout, stderr) => {
+            if (error || stderr) return ctx.replyWithMarkdown("```" + error.message.length ? error.message : "..." || stderr.length ? stderr : "..." + "```", { reply_to_message_id: ctx.message.message_id });
+            else ctx.replyWithMarkdown(`\`\`\`${stdout.length ? stdout : "Executed"}\`\`\``, { reply_to_message_id: ctx.message.message_id });
         });
     }
 }
